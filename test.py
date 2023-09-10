e307e6e14a7b069c2aa002b4f61aefb803ad7172
Cedric Kiplimo <kiplimock@gmail.com>
2023-09-10 08:56:37 +0300

    spacing the four buttons horizontally

--- test.py
+++ test.py
@@ -1,17 +1,37 @@
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.scrollview import ScrollView
-from kivy.core.window import Window
-from kivy.app import runTouchApp
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
 
-layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
-# Make sure the height is such that there is something to scroll.
-layout.bind(minimum_height=layout.setter('height'))
-for i in range(100):
-    label = Label(text="Hello World", size_hint_y=None, height=10)
-    layout.add_widget(label)
+class MyApp(App):
+    def build(self):
+        # Create a horizontal box layout
+        box = BoxLayout(orientation='horizontal')
+        
+        # Create four anchor layouts with different anchors
+        anchor1 = AnchorLayout(anchor_x='left')
+        anchor2 = AnchorLayout(anchor_x='center')
+        anchor3 = AnchorLayout(anchor_x='right')
+        anchor4 = AnchorLayout(anchor_x='center')
+        
+        # Add buttons to each anchor layout
+        btn1 = Button(text='Column 1')
+        btn2 = Button(text='Column 2')
+        btn3 = Button(text='Column 3')
+        btn4 = Button(text='Column 4')
+        
+        anchor1.add_widget(btn1)
+        anchor2.add_widget(btn2)
+        anchor3.add_widget(btn3)
+        anchor4.add_widget(btn4)
+        
+        # Add the anchor layouts to the box layout
+        box.add_widget(anchor1)
+        box.add_widget(anchor2)
+        box.add_widget(anchor3)
+        box.add_widget(anchor4)
+        
+        return box
 
-root = ScrollView(bar_width=12)
-root.add_widget(layout)
-
-runTouchApp(root)
+if __name__ == '__main__':
+    MyApp().run()
